f582a9a0828e1612f6c9bb4eb861c4b12be7894f
swhors@mytalk.co.kr <swhors@mytalk.co.kr>
2020-08-15 15:27:26 +0900

    [DEV-305] change script

--- noti/noti-deploy.sh
+++ noti/noti-deploy.sh
@@ -5,8 +5,9 @@ ENABLED=0
 DEPLOY_FILE=template/bemily-noti-deploy4.yaml
 SERVICE_FILE=template/bemily-noti-service-node_port.yaml
 AUTOSCALE_FILE=template/bemily-noti-autoscaling.yaml
-
 FILES=($DEPLOY_FILE $SERVICE_FILE $AUTOSCALE_FILE)
+SVC_DEP_FILE="bemily-noti-svc-dep.yaml"
+
 
 function find_color {
   line0=$(grep -m1 'color' $SERVICE_FILE)
@@ -33,24 +34,46 @@ function get_next_color {
 }
 
 
+function change_color {
+  for fileName in "${FILES[@]}"
+  do
+    echo $fileName ',' ${1} ',' ${2}
+    sed -e "s/${1}/${2}/g" "${fileName}" > "${fileName}_new"
+    mv "${fileName}_new" "${fileName}"
+  done
+}
+
+
+function create_service {
+  if [ -f "$DEPLOY_FILE" ]; then
+    cat $DEPLOY_FILE > $SVC_DEP_FILE
+    echo "" >> $SVC_DEP_FILE
+    echo "---" >> $SVC_DEP_FILE
+  fi
+
+  if [ -f "$SERVICE_FILE" ]; then
+    cat $SERVICE_FILE >> $SVC_DEP_FILE
+    echo "" >> $SVC_DEP_FILE
+    echo "---" >> $SVC_DEP_FILE
+  fi
+
+  if [ -f "$AUTOSCALE_FILE" ]; then
+    cat $AUTOSCALE_FILE >> $SVC_DEP_FILE
+  fi
+}
+
+
 COLOR=$(find_color)
 
 NEXT_COLOR=$(get_next_color ${COLOR})
 
 echo "CURRENT="$COLOR", NEXT="$NEXT_COLOR
 
-
-for fileName in "${FILES[@]}"
-do
-  echo 'file='$fileName
-  sed -e "s/${COLOR}/${NEXT_COLOR}/g" "${fileName}" > "${fileName}_new"
-  mv "${fileName}_new" "${fileName}"
-done
+change_color ${COLOR} ${NEXT_COLOR}
+create_service
 
 
-OUT_FILE="bemily-noti-deploy.yaml"
 if [ "$ENABLED" -eq 1 ]; then
-  sed -e "s/SYNCVERSION/${SYNCVERSION}/g" "${TMP_FILE}" > "${NEW_FILE}"
-  echo ${NEW_FILE}' is created.'
-  docker-compose up -d syncdaemon
+  kubectl apply -f $SVC_DEP_FILE
 fi
+
